26256fe6bc978e98a5006f1fc247d745eb109674
Marcos Velazquez <marcos.velazquez@highwinds.com>
2016-07-09 17:58:02 -0400

    Fixing RoleMiner to get the id before calling getObjectByID

--- role.miner.js
+++ role.miner.js
@@ -2,11 +2,8 @@ var roleMiner = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
-        console.log(JSON.stringify(creep.memory.assigned_source))
-        console.log(Game.getObjectById(creep.memory.assigned_source))
-        console.log(creep.harvest(Game.getObjectById(creep.memory.assigned_source)))
-        if (creep.harvest(Game.getObjectById(creep.memory.assigned_source)) == ERR_NOT_IN_RANGE){
-			creep.moveTo(Game.getObjectById(creep.memory.assigned_source))
+        if (creep.harvest(Game.getObjectById(creep.memory.assigned_source.id)) == ERR_NOT_IN_RANGE){
+			creep.moveTo(Game.getObjectById(creep.memory.assigned_source.id))
 			}
 		}
 	}
